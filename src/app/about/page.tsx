4eb9c89f4586508a46eb8cf3bf3cae67a7129124
Brian Rigoni <brian.rigoni1@gmail.com>
2023-06-27 02:11:18 +0200

    feat: about information

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,19 @@
 export default function About() {
   return (
     <section>
-      <h1>About</h1>
+      <h1 className="text-3xl font-serif font-bold">About Me</h1>
+      <p className="my-5 max-w-lg text-neutral-800 dark:text-neutral-200">
+        Hey, I&apos;m Ariel Rigoni, Currently, I&apos;m a Full Stack Developer with around 3 to 4 years of experience, primarily focusing on web projects with React.js and mobile applications with React Native. I&apos;ve had the opportunity to work with companies both in Argentina and the U.S., predominantly utilizing TypeScript in my work.
+      </p>
+      <hr className="max-w-lg" />
+      <p className="my-5 max-w-lg text-neutral-800 dark:text-neutral-200">
+        I&apos;m passionate about video games, music, coding, emerging technologies, machine learning, and learning foreign languages. These interests weave together, driving me to push the boundaries of what I can create and learn.
+      </p>
+      <p className="my-5 max-w-lg text-neutral-800 dark:text-neutral-200">
+        I see a future for myself in machine learning, a rapidly growing field that excites me with its limitless potential. As part of this vision, I&apos;m currently finishing my bachelor&apos;s thesis on a mobile application that uses machine learning to recognize flora within my city&apos;s national park.
+        This is just one of the ways I&apos;m integrating my passion for machine learning into real, functional applications, alongside a fitness application and an extension for streaming platforms that I&apos;m currently developing.
+      </p>
+
     </section>
   )
 }
